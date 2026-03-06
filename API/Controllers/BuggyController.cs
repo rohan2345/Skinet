a7a63e2b9fed1ab813faad92adf52254adfceec8
rohan2345 <rohansahani234@gmail.com>
2026-03-06 21:50:06 +0530

    Added registratio/login using asp.net identity

--- API/Controllers/BuggyController.cs
+++ API/Controllers/BuggyController.cs
@@ -1,12 +1,15 @@
 using System;
+using System.Security.Claims;
 using API.DTO;
 using Core.Entities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers;
 
 public class BuggyController :BaseAPIController
 {
+    
     [HttpGet("unauthorized")]
     public IActionResult GetUnauthorized()
     {
@@ -34,6 +37,16 @@ public class BuggyController :BaseAPIController
     public IActionResult GetValidationError(CreateProductDto product)
     {
         return Ok();
+    }
+
+    [Authorize]
+    [HttpGet("secret")]
+    public IActionResult GetSecret()
+    {
+        var name = User.FindFirst(ClaimTypes.Name)?.Value;
+        var id= User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+
+        return Ok("Hello " + name + "with the id of " + id);
     }
 
 }
---
+++ Infrastructure/Migrations/20260306132342_IdentityAdded.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class IdentityAdded : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "LasttName",
+                table: "AspNetUsers",
+                newName: "LastName");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "LastName",
+                table: "AspNetUsers",
+                newName: "LasttName");
+        }
+    }
+}
